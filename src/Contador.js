d035db8bbafe42964b5ef9a3c5b1c622037ffde1
Axel Perossa <adperossa@gmail.com>
2019-09-06 00:05:53 -0300

    fix: porcentaje NaN si total es cero

--- src/Contador.js
+++ src/Contador.js
@@ -1,28 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Contador = props => (
-  <table className="counter">
-    <tbody>
-      <tr>
-        <td>Asistir&aacute;n:</td>
-        <td>{props.confirmados}</td>
-      </tr>
-      <tr>
-        <td>Sin confirmar:</td>
-        <td>{props.sinConfirmar}</td>
-      </tr>
-      <tr>
-        <td>Total:</td>
-        <td>{props.total}</td>
-      </tr>
-      <tr>
-        <td>Presentismo:</td>
-        <td>{Math.round((props.confirmados * 100) / props.total) + ' %'}</td>
-      </tr>
-    </tbody>
-  </table>
-)
+const Contador = props => {
+  const porcentaje = Math.round((props.confirmados * 100) / props.total) + ' %';
+
+  return (
+    <table className="counter">
+      <tbody>
+        <tr>
+          <td>Asistir&aacute;n:</td>
+          <td>{props.confirmados}</td>
+        </tr>
+        <tr>
+          <td>Sin confirmar:</td>
+          <td>{props.sinConfirmar}</td>
+        </tr>
+        <tr>
+          <td>Total:</td>
+          <td>{props.total}</td>
+        </tr>
+        <tr>
+          <td>Presentismo:</td>
+          <td>{props.total > 0 ? porcentaje : '---'}</td>
+        </tr>
+      </tbody>
+    </table>
+  )
+}
 
 Contador.propTypes = {
   confirmados: PropTypes.number.isRequired,
