b6277e07a70a277032e7e9676fbf66743191dec3
Axel Perossa <adperossa@gmail.com>
2019-09-08 23:56:54 -0300

    Updates

---
+++ precache-manifest.24da816af30ad8e771ad4836d4446c36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a31910e910aa61f0182c44698bd1cb67",
+    "url": "/adperossa/rsvp-app/index.html"
+  },
+  {
+    "revision": "cc7be1b8cc5963a1d458",
+    "url": "/adperossa/rsvp-app/static/css/main.f6238a62.chunk.css"
+  },
+  {
+    "revision": "1bd334142197aee3f1ff",
+    "url": "/adperossa/rsvp-app/static/js/2.967d51a9.chunk.js"
+  },
+  {
+    "revision": "cc7be1b8cc5963a1d458",
+    "url": "/adperossa/rsvp-app/static/js/main.ad309faf.chunk.js"
+  },
+  {
+    "revision": "18e719ffc076d2497e92",
+    "url": "/adperossa/rsvp-app/static/js/runtime~main.93a4321a.js"
+  },
+  {
+    "revision": "be92ab00f0f555419adb491aa392b9ca",
+    "url": "/adperossa/rsvp-app/static/media/header-bg.be92ab00.jpg"
+  }
+]);
